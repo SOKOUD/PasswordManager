0b10b5527f6399a0e4b72cae87b15a88e2fa82b1
SOKOUDJOU <lucienchendjou@gmail.com>
2022-07-13 10:44:30 +0100

    changes in repository

--- accounts.h
+++
@@ -1,9 +0,0 @@
-#ifndef accounts_H
-#define accounts_H
-#include "Systeme.h"
-
-void systemMenu();
-void userMenu(char name[]);
-void adminMenu();
-SystemPtr connectionAccount(SystemPtr Sys);
-#endif // accounts_H
